ca7785c52d82b47ea8de1690968804a8a53690be
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2023-01-09 14:58:19 -0300

    Add files via upload

---
+++ Practica1_Ejercicio10.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+public class Practica1_Ejercicio10 {
+    public static void main(String[] args){
+        
+        double precio_total = 0;
+
+        for(int i=0; i<4; i++){
+            
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre del producto");
+            
+            int  cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de productos: "));
+            
+            double precio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio de productos:"));
+            
+            double precio_producto = cantidad * precio;
+            
+            System.out.println("El precio de " + nombre + " es $"+ precio_producto); 
+            
+            precio_total = precio_total + precio_producto;
+                
+            System.out.println("El precio total es: $" + precio_total);
+               
+        }  
+    }
+}
+
