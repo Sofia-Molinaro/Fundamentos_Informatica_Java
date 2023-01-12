6d5bd6e87892f078caa78f16a5d7de9315339d9c
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2023-01-12 17:25:21 -0300

    Add files via upload

---
+++ Practica1_Ejercicio19.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Practica1_Ejercicio19 {
+    public static void main(String[] args){
+        List<String> ejemploLista = new ArrayList<>();
+
+        ejemploLista.add("Lunes");
+        ejemploLista.add("Martes");
+        ejemploLista.add("Miércoles");
+        ejemploLista.add("Jueves");
+        ejemploLista.add("Viernes");
+        ejemploLista.add("Sábado");
+        ejemploLista.add("Domingo");
+        
+        System.out.println("Los días de la semana son: " + ejemploLista);
+    }
+}
