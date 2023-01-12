e0cb99b4b52fde231ade60a6cb71b57a1eeb0ff4
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2023-01-11 22:44:14 -0300

    Add files via upload

---
+++ Practica1_Ejercicio18.java
@@ -0,0 +1,8 @@
+//Mostrar 5 veces la cadena “Hola”.
+public class Practica1_Ejercicio18 {
+    public static void main(String[] args){
+        for(int i=0; i<5; i++){
+            System.out.println("Hola");
+        }
+    }
+}
