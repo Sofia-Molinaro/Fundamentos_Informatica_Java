285ddf65cb7297bffe3a032a5a13fb200dacad01
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2023-01-12 17:29:10 -0300

    Add files via upload

---
+++ Practica1_Ejercicio20.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Practica1_Ejercicio20 {
+    public static void main(String[] args){
+        
+        List<String> acontecimientos = new ArrayList<>();
+       
+        acontecimientos.add("Guerra del Peloponeso 431 a.C");
+       
+        acontecimientos.add("Comienzo de la construcción de la gran Muralla China 214 a.C");    
+       
+        acontecimientos.add("Llegada de los españoles a América 1492 d.C");
+        
+        acontecimientos.add("Revolución de Mayo 1810 d.C");
+    }
+}
