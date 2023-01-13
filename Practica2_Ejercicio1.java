61a8214225f15896e310812fe3292667bab174df
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2023-01-12 22:06:24 -0300

    Add files via upload

---
+++ Practica2_Ejercicio1.java
@@ -0,0 +1,21 @@
+//Definir una función denominada imprimir_mensaje que retorne e imprima el siguiente mensaje en pantalla: “Estudiando Fundamentos de Informática en la UNAJ”.
+
+public class Practica2_Ejercicio1 {
+    public static String imprimir_mensaje(String mensaje){
+        
+        return mensaje;
+    }
+    public static void main(String[] args){
+        String m, resultado;
+
+        m = "Estudiando Fundamentos de Informática en la UNAJ";
+        
+        resultado = imprimir_mensaje(m);
+       
+        System.out.println(resultado);
+    }
+
+    
+
+}
+ 
